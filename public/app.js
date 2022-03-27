891f9c8ca1bb117f76146d548be874fd5e551d20
uzair abid <uzairabid1001@gmial.com>
2022-03-27 22:01:47 +0500

    first commit

---
+++ public/app.js
@@ -0,0 +1,17 @@
+let a = {
+    Name: "Uzair Abid",
+    Fathername:"M.Abid",
+    Age : 16,
+    Class: "XI"
+}
+
+console.log(Object.keys(a))
+
+let b = {
+    Name: "Uzair Abid",
+    Fathername:"M.Abid",
+    Age : 16,
+    Class: "XI"
+}
+
+console.log(Object.values(b))
